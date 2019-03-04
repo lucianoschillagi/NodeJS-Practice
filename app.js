15f7bb51611ef1dd628d5f1d7a5df157052b150e
luko <luko.web@gmail.com>
2019-03-04 18:05:11 -0300

    Add file system module example, part two

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+// file system
 
 const fs = require('fs');
 
 // const files = fs.readdirSync('./');
 // console.log(files);
 
-fs.readdir('./', function(err, files) {
+fs.readdir('$', function(err, files) {
  if (err) console.log('Error', err);
  else console.log('Result', files);
 });
